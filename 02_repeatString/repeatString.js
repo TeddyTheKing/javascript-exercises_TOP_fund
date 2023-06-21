1c2c00708c33e5347ad8b53a5bebcb657a57baa4
TeddyTheKing <heldinho17@gmail.com>
2023-06-21 20:31:07 +0200

    Finish first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let multi = "";
+    for(let i = 0; i < num; i++) {
+        multi += str;
+    };
 
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        return multi;
+    }    
 };
 
 // Do not edit below this line
