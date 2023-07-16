46c29d479ec20692212c80cb6fa450ccd3864ca3
TeddyTheKing <heldinho17@gmail.com>
2023-07-16 19:28:39 +0200

    Finish sumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArr, spec) {
+    let newArr = [];
+    for (let i = 1; i < myArr.length; i++) {
+        if (i !== spec) {
+            newArr.push(i);
+        } else {
+            continue;
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    suma = 0;
+    if (a > b) {
+        for (let i = b; i <= a; i++) {
+            suma += i;
+        };
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    } else {
+        for (let i = a; i <= b; i++) {
+        suma += i;
+    }
+}
+return suma;
 };
 
 // Do not edit below this line
