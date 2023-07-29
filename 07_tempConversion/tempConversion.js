007fb83c31e750627007de5e7aa49ff67392bb54
TeddyTheKing <heldinho17@gmail.com>
2023-07-29 16:50:43 +0200

    Finish tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  let celsius = (a - 32) * 5/9;
+  return Math.round(celsius * 10) / 10;
+  return zaokcel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(b) {
+  let fahrenheit = (b * 9/5) + 32;
+  let zaokfah = Math.round(fahrenheit * 10) / 10;
+  return zaokfah
 };
 
 // Do not edit below this line
